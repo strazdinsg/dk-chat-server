3f0fad4cb9b66bbb261f80851b32c2d9e8004cf0
Jonathan Oie <jonoie95@gmail.com>
2018-09-10 16:06:14 +0200

    Step 8: Make a collection where you will store all active connections.

--- src/NtnuChatServer/Server.java
+++ src/NtnuChatServer/Server.java
@@ -3,6 +3,7 @@ package NtnuChatServer;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -12,11 +13,14 @@ import java.util.logging.Logger;
  */
 public class Server {
     private ServerSocket serverSocket;
+    private HashMap<Integer, ClientHandler> connectedClients;
     
     /**
      * @param port the server port
      */
     public Server(int port){
+        connectedClients = new HashMap<Integer, ClientHandler>();
+        
         try {
             // Start listening on port
             serverSocket = new ServerSocket(port);
@@ -36,6 +40,10 @@ public class Server {
                 
                 // Start thread
                 clientHandler.start();
+                
+                // Add thread to connectedClients hashtable
+                int clientHandlerThreadId = (int)clientHandler.getId();
+                connectedClients.put(clientHandlerThreadId, clientHandler);
             
             } catch (IOException ex) {
                 Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
