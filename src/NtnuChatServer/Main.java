12a87fd9d563b14e9d3e13dabbca1a2e0e208a0a
Girts <gist@ntnu.no>
2019-07-18 16:19:19 +0200

    Allow to specify port number in the command line
    Ignore some build files

--- src/NtnuChatServer/Main.java
+++ src/NtnuChatServer/Main.java
@@ -5,12 +5,24 @@ package NtnuChatServer;
  * Used as part of assignment A4 in DataKomm course.
  */
 public class Main {
+    // Default TCP Port to use
+    private static final int DEFAULT_PORT = 1300;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Start a new server on port 1300
+        // Start a new server on a given port. Use 1300 as default port
+        int port = DEFAULT_PORT;
+        if (args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid port specified: " + args[0]);
+                port = DEFAULT_PORT;
+            }
+        }
         Server server = new Server();
-        server.start(1300);
+        server.start(port);
     }
 }
