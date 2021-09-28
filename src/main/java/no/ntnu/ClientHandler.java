add6588c495b779e181405d275694b3107b9d1fc
Girts <girts.strazdins@gmail.com>
2021-09-28 12:39:43 +0200

    Step 9: read clients message, log it

--- src/main/java/no/ntnu/ClientHandler.java
+++ src/main/java/no/ntnu/ClientHandler.java
@@ -1,6 +1,8 @@
 package no.ntnu;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 /**
@@ -9,31 +11,62 @@ import java.net.Socket;
 public class ClientHandler extends Thread {
     private final Socket socket;
     private final Server server;
+    private boolean needToRun = true;
+    private final BufferedReader inFromClient;
 
     /**
-     * Constructor
+     * ClientHandler constructor
      *
      * @param clientSocket Socket for this particular client
-     * @param server The main server class which manages all the connections
+     * @param server       The main server class which manages all the connections
      */
     public ClientHandler(Socket clientSocket, Server server) {
         this.socket = clientSocket;
         this.server = server;
+        this.inFromClient = createInputStreamReader();
+    }
+
+    /**
+     * Create buffered input stream reader for the socket
+     * @return The input stream reader or null on error
+     */
+    private BufferedReader createInputStreamReader() {
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (IOException e) {
+            Server.log("Could not setup the input stream!");
+        }
+        return reader;
     }
 
     /**
      * Handle the conversation according to the protocol
      */
     public void run() {
-        Server.log("Emulating a long operation...");
-        try {
-            Thread.sleep(10 * 1000);
-        } catch (InterruptedException e) {
-            Server.log("Processing interrupted");
+        while (needToRun) {
+            String message = readClientMessage();
+            if (message != null) {
+                Server.log(getId() + ": " + message);
+            }
         }
         Server.log("Done processing client");
         closeSocket();
         server.removeClientHandler(this);
+    }
+
+    /**
+     * Read one message from the client (from the socket)
+     * @return The message or null on error
+     */
+    private String readClientMessage() {
+        String message = null;
+        try {
+            message = inFromClient.readLine();
+        } catch (IOException e) {
+            Server.log("Error while reading the socket input: " + e.getMessage());
+        }
+        return message;
     }
 
     /**
