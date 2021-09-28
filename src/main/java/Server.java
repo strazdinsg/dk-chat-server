89c854c8331300ef693d25dc885b988f2393f740
Girts <girts.strazdins@gmail.com>
2021-09-28 09:52:09 +0200

    Steps 3-4 implemented: accept clients in a loop

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -19,19 +19,79 @@ public class Server {
      * Start the TCP chat server
      */
     private void run() {
+        ServerSocket welcomeSocket = openWelcomingSocket();
+        if (welcomeSocket != null) {
+            while (true) {
+                Socket clientSocket = acceptNextClient(welcomeSocket);
+                if (clientSocket != null) {
+                    handleClient(clientSocket);
+                }
+            }
+        }
+    }
+
+    /**
+     * Open a server socket which listens on the predefined TCP port
+     *
+     * @return the welcoming server socket, null on error
+     */
+    private ServerSocket openWelcomingSocket() {
+        ServerSocket welcomeSocket = null;
         try {
-            ServerSocket welcomeSocket = new ServerSocket(TCP_PORT);
+            welcomeSocket = new ServerSocket(TCP_PORT);
             log("Server listening on port " + TCP_PORT);
-
-            Socket clientSocket = welcomeSocket.accept();
-            log("New client connected from " + clientSocket.getRemoteSocketAddress());
-
-            log("Closing client socket...");
-            clientSocket.close();
-            log("Client socket closed");
         } catch (IOException e) {
             log("Could not open a listening socket: " + e.getMessage());
         }
+        return welcomeSocket;
+    }
+
+    /**
+     * Block execution until the next client connects
+     *
+     * @param welcomeSocket The listening server socket
+     * @return Socket for the newly connected client, null on error
+     */
+    private Socket acceptNextClient(ServerSocket welcomeSocket) {
+        Socket clientSocket = null;
+        try {
+            clientSocket = welcomeSocket.accept();
+            log("New client connected from " + clientSocket.getRemoteSocketAddress());
+        } catch (IOException e) {
+            log("Failed to accept a client connection: " + e.getMessage());
+        }
+        return clientSocket;
+    }
+
+    /**
+     * Handle one client connection - all the server logic
+     *
+     * @param clientSocket The socket for this client
+     */
+    private void handleClient(Socket clientSocket) {
+        log("Emulating a long operation...");
+        try {
+            Thread.sleep(10 * 1000);
+        } catch (InterruptedException e) {
+            log("Processing interrupted");
+        }
+        log("Done processing client");
+        closeClientSocket(clientSocket);
+    }
+
+    /**
+     * Close connection for the particular client
+     *
+     * @param clientSocket The socket to close
+     */
+    private void closeClientSocket(Socket clientSocket) {
+        log("Closing client socket...");
+        try {
+            clientSocket.close();
+        } catch (IOException e) {
+            log("Error while closing a client socket: " + e.getMessage());
+        }
+        log("Client socket closed");
     }
 
     /**
